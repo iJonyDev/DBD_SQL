a3b39101ee3dc45aa6faa4d596849c04300925da
iDeadtroll <javielproductor@gmail.com>
2023-11-28 19:47:40 +0100

    EPD4 V2

--- EPD4/Esperimento.sql
+++ EPD4/Esperimento.sql
@@ -9,10 +9,18 @@ or last_name like '%UR%'
 or last_name like '%ur%';
 */
 
+SELECT first_name, last_name
+from HR.employees
+WHERE LOWER(LAST_NAME) LIKE '%ur%';
+
 /*
 E2. Obtenga una lista de todos los valores de los atributos FIRST_NAME y LAST_NAME de la tabla EMPLOYEES cuyo
 FIRST_NAME contenga la cadena “li”. Tiempo estimado: 10 minutos
 */
+
+SELECT FIRST_NAME, LAST_NAME 
+FROM HR.EMPLOYEES
+WHERE FIRST_NAME LIKE '%li%';
 
 /*
 E3.Obtenga a partir de la tabla EMPLOYEES aquellos trabajadores que pertenezcan al departamento identificado con el número
@@ -20,6 +28,9 @@ E3.Obtenga a partir de la tabla EMPLOYEES aquellos trabajadores que pertenezcan 
 función CONCAT. Tiempo estimado: 10 minutos
 */
 
+SELECT FIRST_NAME||' '||LAST_NAME||' gana ' || SALARY||'$' "INFORMACION"
+FROM HR.EMPLOYEES
+WHERE DEPARTMENT_ID = 100;
 
 /*
 E4. A partir de la tabla EMPLOYEES muestre una cadena que contenga la inicial del nombre, el símbolo de puntuación ‘.’ y el
@@ -28,10 +39,18 @@ ejemplo, si tenemos almacenado un trabajador que cumpla la condición impuesta y
 LAST_NAME='Pérez', se deberá mostrar 'L.Pérez'. Tiempo estimado: 10 minutos
 */
 
+SELECT INITCAP(FIRST_NAME)||'.'||LAST_NAME
+FROM HR.EMPLOYEES
+WHERE JOB_ID LIKE 'AD%';
+
 /*
 E5. Modifique la consulta realizada en el experimento E4 de forma que en el caso de existir un apellido compuesto se eliminen los
 espacios en blanco existentes. Tiempo estimado: 10 minutos
 */
+
+SELECT INITCAP(FIRST_NAME)||'.'||REPLACE(LAST_NAME,' ','') "NAMES"
+FROM HR.EMPLOYEES
+WHERE JOB_ID LIKE 'AD%';
 
 /*
 E6. Supongamos que el campo SALARY de la tabla EMPLOYEES se refiere al sueldo anual de cada trabajador. La empresa ha
@@ -40,16 +59,32 @@ mensual van a recurrir a la división entera. Muestre los datos de los trabajado
 anualmente de los trabajadores. Tiempo estimado: 10 minutos
 */
 
+SELECT  FIRST_NAME, 
+        LAST_NAME ,
+        TRUNC((SALARY/12)) "MENSUAL_SIN_DECIMALES", 
+        SALARY-(TRUNC((SALARY/12))*12) "ANUAL NO PERCIBIDA"
+FROM HR.EMPLOYEES;
+
 /*
 E7. A partir de la tabla JOBS, muestre los datos de trabajo realizado (JOB_TITLE) y salario máximo ganado (MAX_SALARY) de
 forma que se muestre este último campo con el símbolo del dólar, como separador de millares una coma y como separador de
 decimales un punto (por ej.: $10,020.45). Tiempo estimado: 5 minutos
 */
 
+SELECT JOB_TITLE, TO_CHAR(MAX_SALARY,'$99,999.99') "MAX SALARY"
+FROM HR.JOBS;
+
 /*
 E8. Mostrar la fecha del sistema de forma que si la fecha es 1/11/2011, martes, deberá mostrar por pantalla “Hoy es LUNES, 01 de
 NOVIEMBRE de 2011”. Tiempo estimado: 10 minutos
 */
+
+ALTER SESSION SET NLS_LANGUAGE = 'SPANISH'; --Cambiamos el idioma de la sesion a Español
+SELECT 'Hoy es '||TO_CHAR(SYSDATE-1,' DAY') --TO_CHAR(dia de la semana -1)
+    	||' de '||TO_CHAR(SYSDATE,'MONTH')  --TO_CHAR(cadena mes completo)
+    	||' de '||TO_CHAR(SYSDATE,'YYYY')   --TO_CHAR(año con 4 digitos)
+    FROM DUAL;
+
 
 /*
 E9. De cada empleado contratado a partir del 12 de Enero 2000 se desea mostrar el nombre (FIRST_NAME), apellido
