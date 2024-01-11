6b7bb4073c95cc62156735d5345cfaabe868cf21
iDeadtroll <javielproductor@gmail.com>
2024-01-11 14:05:44 +0100

    Practica Examen DBD v1.2

--- ModeloExamenEnero/AlgebraRelaciona.sql
+++ ModeloExamenEnero/AlgebraRelaciona.sql
@@ -6,22 +6,44 @@
 -- 1) Encontrar la informacion de todos los prestamos realizados en MERIDA
 	    σ nombresucursal = 'MERIDA' (prestamo ⨝ sucursal)
 	
--- 2) Determinar el nombre de los lientes que viden en OAXACA
+-- 2) Determinar el nombre de los lientes que viven en OAXACA
 	    σ estado = 'OAXACA' (cliente)
 
 -- 3) Nombre de todos los clientes del banco que tienen una cuenta, un prestamo o ambas
 	-- Utilizando operadores de conjunto
-	    π nombrecliente (cliente	⨝ (ctacliente ∪ prestatario))
+	    π nombrecliente (cliente ⨝ (ctacliente ∪ prestatario))
 	
 -- 4) Relacion de los clientes que tienen abierta una cuenta, pero no tienen ninguna de credito
 		-- Por resta o diferencia de conjuntos
 		    cliente ⨝ (π idcliente ctacliente - π idcliente prestatario)
 		-- Usando JOIN EXTERNO
-		    -- ctacliente ⨝ prestatario
+		    -- ctacliente ⨝ prestatario NATURAL JOIN
+			-- ctacliente ⟕ prestatario LEFT JOIN
 		    π nombrecliente, numcta(cliente ⨝ ((ctacliente ⟕ prestatario) - (ctacliente ⨝ prestatario)))
-		
 
--- Todos los clientes que tienen un prestamo y una cuenta abierta
--- Nombre de todos los clientes que tienen un credito en la sucursal TULUM
--- Nombre de todos los clientes que tienen un prestamo y el importe del mismo
--- El nombre de todas las sucursales con clientes que tienen una cuenta abierta en el banco y que viven en CHIAPAS
+-- 5) Todos los clientes que tienen un prestamo y una cuenta abierta
+		-- Por Interseccion de cojuntos
+			π nombrecliente, idcliente(cliente ⨝ (π idcliente (ctacliente) ∩ π idcliente (prestatario)))
+		-- Por NATURAL JOIN
+			π idcliente, nombrecliente, numcta, numprestamo(cliente ⨝ (ctacliente ⨝ prestatario))
+
+-- 6) Nombre de todos los clientes que tienen un credito en la sucursal TULUM
+		-- Primera forma:
+			r = π idcliente,nombrecliente (cliente)
+			σ nombresucursal = 'TULUM'( r	⨝ prestatario ⨝ prestamo ⨝ sucursal)
+		-- Segunda forma:
+			r = π idcliente,nombrecliente (cliente)
+			r ⨝ prestatario ⨝ prestamo ⨝ (σ nombresucursal = 'TULUM' (sucursal))
+
+-- 7) Nombre de todos los clientes que tienen un prestamo y el importe del mismo
+		-- Forma facil con NATURAL JOIN
+ 			π nombrecliente,numprestamo,importe(cliente ⨝ prestatario ⨝ prestamo)
+		-- Con THETA JOIN
+			r = cliente ⨝ cliente.idcliente = prestatario.idcliente prestatario
+			s = r ⨝ prestatario.numprestamo = prestamo.numprestamo (prestamo)
+			π cliente.nombrecliente,prestatario.numprestamo,prestamo.importe (s)
+
+-- 8) El nombre de todas las sucursales con clientes que tienen una cuenta abierta en el banco y que viven en CHIAPAS
+
+			r = π idcliente,nombrecliente (σ estado = 'CHIAPAS' (cliente))
+			π nombresucursal(r ⨝ ctacliente ⨝ cuenta ⨝ sucursal)
